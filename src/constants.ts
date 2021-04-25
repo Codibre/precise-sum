8d4dd4f4dddcc902ccf455ff32611eafe3d2cabf
Thiago Santos <tos.oliveira@gmail.com>
2021-04-25 13:38:54 -0300

    creating bigint fallback multiplication

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 /* eslint-disable no-magic-numbers */
 export const defaultDigits = 3;
 export const defaultCorrectionFactor = 10 ** defaultDigits;
+export const maxSafeInteger = BigInt(Number.MAX_SAFE_INTEGER);
+export const maxSafeFactor = 1e24;
--- src/multiply.ts
+++ src/multiply.ts
@@ -1,4 +1,43 @@
-import { defaultCorrectionFactor } from './constants';
+import {
+	defaultCorrectionFactor,
+	maxSafeFactor,
+	maxSafeInteger,
+} from './constants';
+
+function convertBack(acc: bigint, correction: bigint, factor: bigint): number {
+	const q = acc / correction;
+	const r = acc % correction;
+
+	return (
+		Number(q) +
+		(r > maxSafeInteger
+			? convertBack(r, correction / factor, factor)
+			: Number(r) / Number(correction))
+	);
+}
+
+function bigIntFallback(
+	init: number,
+	values: (string | number)[],
+	accBase: number,
+	correctionBase: number,
+	correctionFactorBase: number,
+) {
+	let acc = BigInt(Math.round(accBase));
+	let correction = BigInt(correctionBase);
+	const correctionFactor = BigInt(correctionFactorBase);
+
+	for (let i = init; i < values.length; i++) {
+		acc *= BigInt((values[i] as number) * correctionFactorBase);
+		if (acc % correctionFactor !== 0n) {
+			correction *= correctionFactor;
+		} else {
+			acc /= correctionFactor;
+		}
+	}
+
+	return convertBack(acc, correction, correctionFactor);
+}
 
 /**
  * Sums the informed value applying the correction factor for better precision
@@ -14,11 +53,20 @@ export function multiply(
 	let acc = 1;
 	let correction = 1;
 	for (let i = 0; i < length; i++) {
-		acc *= (values[i] as number) * correctionFactor;
-		if (acc % correctionFactor !== 0) {
-			correction *= correctionFactor;
-		} else {
+		const newValue = (values[i] as number) * correctionFactor;
+		const result = acc * newValue;
+		if (result > Number.MAX_SAFE_INTEGER) {
+			return bigIntFallback(i, values, acc, correction, correctionFactor);
+		}
+		acc = result;
+		if (acc % correctionFactor === 0) {
 			acc /= correctionFactor;
+		} else {
+			const newCorrection = correction * correctionFactor;
+			if (newCorrection > maxSafeFactor) {
+				return bigIntFallback(i, values, acc, correction, correctionFactor);
+			}
+			correction = newCorrection;
 		}
 	}
 	return acc / correction;
